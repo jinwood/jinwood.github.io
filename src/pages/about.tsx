5439d4cd174b4c9dc95a54301da7448c45bae06c
jinwood <jinw@protonmail.com>
2020-07-08 19:53:07 +0100

    Content

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -16,31 +16,44 @@ const About = () => {
           <p>
             <strong>
               A programming enthusiast who takes pride in writing good software.
-            </strong>
-            {" "}
+            </strong>{" "}
             I'm experienced in web development and have worked across a variety
             of stacks, in various industries. I started my career working in
             ASP.NET but have since transitioned into more JavaScript orintated
             positions. With that said, I am interested in any and all tech
             stacks.
           </p>
-          <h3>
-            Employment
-          </h3>
+        </Col>
+      </Row>
+      <Row>
+        <Col md={8}>
+          <p>
+            Currently I am working with react, redux and flow during the day,
+            and TypeScript at home. I'm experienced with C# .NET and spent the
+            first few years of my software career building web apps using this
+            technology. Although currently my preference is with the JavaScript
+            / node ecosystem, I am interested in any and all web technologies.
+          </p>
+        </Col>
+      </Row>
+      <Row>
+        <Col md={8}>
+          <h3>Employment</h3>
           <h4>Current</h4>
           <strong>Quadrotech</strong>
           <p>
-            I currently work for <a
-              href="https://www.quadrotech-it.com/"
-              target="blank"
-            >
+            I currently work for{" "}
+            <a href="https://www.quadrotech-it.com/" target="blank">
               Quadrotech
-            </a>, building <a
+            </a>
+            , building{" "}
+            <a
               href="https://www.quadrotech-it.com/office-365-management-software/"
               target="blank"
             >
               Nova
-            </a>, an Office 365 management and reporting platform for the
+            </a>
+            , an Office 365 management and reporting platform for the
             enterprise. I'm part of the UI team working with React, Redux and
             Flow. My day to day activities include building new features, fixing
             bugs, writing documentation, tests, etc. I am currenty spearheading
@@ -59,12 +72,14 @@ const About = () => {
               the Just Eat web platform
             </li>
             <li>
-              Contribute to <a
+              Contribute to{" "}
+              <a
                 href="https://github.com/justeat/fozzie/graphs/contributors"
                 target="blank"
               >
                 Fozzy
-              </a>, UI library and suite of tools used across Just Eat.
+              </a>
+              , UI library and suite of tools used across Just Eat.
             </li>
             <li>
               Assist in migration from classic MVC style UI archtecture to Vue
@@ -82,17 +97,6 @@ const About = () => {
               call support, montioring, escaltion and issue resolution
             </li>
           </ul>
-        </Col>
-      </Row>
-      <Row>
-        <Col md={8}>
-          <p>
-            Currently I am working with react, redux and flow during the day,
-            and TypeScript at home. I'm experienced with C# .NET and spent the
-            first few years of my software career building web apps using this
-            technology. Although currently my preference is with the JavaScript
-            / node ecosystem, I am interested in any and all web technologies.
-          </p>
         </Col>
       </Row>
     </Layout>
