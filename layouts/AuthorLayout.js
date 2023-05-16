233ac298842d21ded9a93eebd8c8e67c5dd8b860
jinwood <jinw@protonmail.com>
2023-05-16 18:52:02 +0100

    Add a print only section to aabout

--- layouts/AuthorLayout.js
+++ layouts/AuthorLayout.js
@@ -9,24 +9,50 @@ export default function AuthorLayout({ children, frontMatter }) {
     <>
       <PageSEO title={`About - ${name}`} description={`About me - ${name}`} />
       <div className="divide-y divide-gray-200 dark:divide-gray-700">
-        <div className="space-y-2 pb-8 pt-6 md:space-y-5">
+        <div className="space-y-2 pb-8 pt-6 print:hidden md:space-y-5">
           <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
             About
           </h1>
         </div>
         <div className="items-start space-y-2 xl:grid xl:grid-cols-3 xl:gap-x-8 xl:space-y-0">
-          <div className="flex flex-col items-center pt-8">
+          {/* hide normally but show when printing */}
+          <div className="hidden print:block">
+            <h1 className="mb-4 text-2xl font-bold">Julian Inwood</h1>
+            <div className="grid grid-cols-2 gap-4">
+              <div>
+                <p className="font-bold">Job Title:</p>
+                <p className="mb-2">Full stack developer</p>
+
+                <p className="font-bold">Company:</p>
+                <p className="mb-2">Cuckoo Internet</p>
+                <p className="font-bold">Current Location:</p>
+                <p className="mb-2">Somerset, UK</p>
+              </div>
+
+              <div>
+                <p className="font-bold">Email:</p>
+                <p className="mb-2">jinw@protonmail.com</p>
+
+                <p className="font-bold">Website:</p>
+                <p className="mb-2">jinwood.github.io</p>
+
+                <p className="font-bold">Key Skills:</p>
+                <p>JavaScript, TypeScript, Node.js, React.js, Next.js, AWS</p>
+              </div>
+            </div>
+          </div>
+          <div className="flex flex-col items-center pt-8 print:hidden">
             <Image
               src={avatar}
               alt="avatar"
               width="192px"
               height="192px"
-              className="h-48 w-48 rounded-full"
+              className="h-48 w-48 rounded-full print:hidden"
             />
             <h3 className="pb-2 pt-4 text-2xl font-bold leading-8 tracking-tight">{name}</h3>
             <div className="text-gray-500 dark:text-gray-400">{occupation}</div>
             <div className="text-gray-500 dark:text-gray-400">{company}</div>
-            <div className="flex space-x-3 pt-6">
+            <div className="flex space-x-3 pt-6 print:hidden">
               <SocialIcon kind="mail" href={`mailto:${email}`} />
               <SocialIcon kind="github" href={github} />
               <SocialIcon kind="linkedin" href={linkedin} />
