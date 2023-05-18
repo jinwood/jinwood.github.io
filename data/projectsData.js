ff5dd1cc51343a7ee83da119e93b7f05f0a398ed
jinwood <jinw@protonmail.com>
2023-05-18 21:10:26 +0100

    Projects and other stuff

--- data/projectsData.js
+++ data/projectsData.js
@@ -2,17 +2,26 @@ const projectsData = [
   {
     title: 'Cuckoo Blog',
     description: `My first major project at Cuckoo internet was implementing a complete redesign of the'
-    company blog. This involved working with the marketing & design teams to bring their vision to life.`,
+    company blog. This involved working with the marketing & design teams to bring their vision to life. This was built using our own homegrown UI library, yolk - https://design.cuckoo.co`,
     imgSrc: '/static/images/cuckoo-blog-home.png',
     href: 'https://www.cuckoo.co/blog',
   },
   {
-    title: 'The Time Machine',
-    description: `Imagine being able to travel back in time or to the future. Simple turn the knob
-    to the desired date and press "Go". No more worrying about lost keys or
-    forgotten headphones with this simple yet affordable solution.`,
-    imgSrc: '/static/images/time-machine.jpg',
-    href: '/blog/the-time-machine',
+    title: 'LetMe CRM',
+    description: `Designed and developed a robust CRM system for LetMe - a new take on the lettings industry. Implemented a microservices architecture to ensure scalability and manageability of customer interactions, sales data, marketing campaigns and user comms. Leveraged Azure cloud services for seamless integration and high availability.`,
+    imgSrc: '/static/images/letme.png',
+  },
+  {
+    title: 'Quadrotech Nova',
+    description: `Quadrotech Nova by Quest is a powerful Office 365 management software built using React, Redux, and TypeScript. With its modern technology stack, Nova provides a user-friendly interface and seamless integration of functionalities. One of its standout features is the robust report building suite, which allows users to create customized reports using a custom API query language. This feature empowers administrators to extract valuable insights and analyze data specific to their organization's needs. By leveraging the flexibility of the custom API query language, Nova enables users to generate comprehensive reports that cover various aspects of Office 365, enhancing decision-making and optimizing operational efficiency.`,
+    imgSrc: '/static/images/nova.png',
+    href: 'https://www.quadrotech-it.com/products/quadrotech-nova/',
+  },
+  {
+    title: 'Just Eat - Global Homepage',
+    description: `As part of the Global Homepage team at Just Eat, I contributed to the maintenance and enhancement of the homepage serving millions of daily requests. Working with the Restaurant Management vertical, I focused on developing new features and optimizing UI components using a modern tech stack, including Micro Frontend architectures, Vue.js, Typescript, Cypress, GitHub/Gitlab CI, and AWS. By adhering to coding standards and collaborating with the team, I ensured a seamless user experience and delivered a high-performance homepage for Just Eat's global platform. This page was built with fozzy, our UI library - https://github.com/justeattakeaway/fozzie-components`,
+    imgSrc: '/static/images/just-eat.png',
+    href: 'https://www.quadrotech-it.com/products/quadrotech-nova/',
   },
 ]
 
