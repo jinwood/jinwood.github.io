543e4eb74bc68965d1a2f276c2b5d023e32334f7
jinwood <jinw@protonmail.com>
2023-09-04 08:59:11 +0100

    Further tweaks

--- components/Gallery.js
+++ components/Gallery.js
@@ -53,18 +53,18 @@ export default function Gallery({ images }) {
     <div>
       <Icon kind="gallery" onClick={openModal} />
       {showModal && (
-        <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-80">
+        <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-black bg-opacity-80">
           <button
             onClick={closeModal}
             className="absolute right-4 top-4 z-50 text-3xl text-white focus:outline-none"
           >
             &times;
           </button>
-          <div className="absolute left-4 top-1/2 z-50 h-20 w-20 cursor-pointer">
-            <button
-              onClick={prevImage}
-              className="absolute left-4 top-1/2 z-50 -translate-y-1/2 transform text-3xl text-white focus:outline-none"
-            >
+          <div
+            onClick={prevImage}
+            className="absolute left-4 top-1/2 z-50 flex h-20 w-20 cursor-pointer items-center justify-center rounded bg-black"
+          >
+            <button onClick={prevImage} className="z-50 text-3xl text-white focus:outline-none">
               &#10094;
             </button>
           </div>
@@ -76,10 +76,10 @@ export default function Gallery({ images }) {
           />
 
           <div
-            className="absolute right-4 top-1/2 z-50 h-20 w-20 cursor-pointer"
+            className="absolute right-4 top-1/2 z-50 flex h-20 w-20 cursor-pointer items-center justify-center rounded-sm bg-black"
             onClick={nextImage}
           >
-            <button className="absolute right-4 top-1/2 z-50 -translate-y-1/2 transform text-3xl text-white focus:outline-none">
+            <button className="text-3xl text-white focus:outline-none" onClick={nextImage}>
               &#10095;
             </button>
           </div>
