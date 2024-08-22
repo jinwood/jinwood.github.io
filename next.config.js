ad0d11818ab87aaa11e53e9cb79e2d3f15082411
jinwood <jinw@protonmail.com>
2024-08-22 19:58:40 +0100

    Build debug

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  trailingSlash: true,
   reactStrictMode: true,
   pageExtensions: ['js', 'jsx', 'md', 'mdx'],
   eslint: {
